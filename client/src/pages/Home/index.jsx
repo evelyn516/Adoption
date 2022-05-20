bd4531c1705dfe82ffa9d7f19d812c3724aa4697
LuizWebDev <luizwagnermelo@hotmail.com>
2022-05-20 12:52:46 +0100

    random changes

---
+++ client/src/pages/Home/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+function Home() {
+  return (
+    <>
+     <h1>Welcome to our Home Page</h1>
+    </>
+  );
+}
+
+export default Home;
---
+++ client/src/pages/SeeAll/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+function SeeAll() {
+  return (
+    <>
+     <h1>All pets to be taken</h1>
+    </>
+  );
+}
+
+export default SeeAll;
