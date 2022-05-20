9866737cab6c260d8a38e952404ab6f2c8412fab
LuizWebDev <luizwagnermelo@hotmail.com>
2022-05-20 12:51:21 +0100

    added the initial routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,25 @@
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Quiz from './pages/Quiz';
+import SeeAll from './pages/SeeAll';
+import NavBar from './components/NavBar/index';
 import './App.css';
 
 function App() {
   return (
    <>
-   <h1>welcome to our adoption app</h1> 
+   {/* <NavBar /> */}
+<main>
+<Routes>
+<Route path="/" element={<Home />} />
+<Route path="/quiz" element={<Quiz />} />
+<Route path="/seeall" element={<SeeAll />} />
+
+</Routes>
+
+
+</main>
+   
    
    </>
   )
