93db621d0522a23b66ef0f21dfafdf4fb6ae0d35
Evie <evie.ball32@gmail.com>
2022-05-24 12:24:55 +0100

    collapsible component added

--- client/src/components/AnimalProfile/index.js
+++ client/src/components/AnimalProfile/index.js
@@ -1,9 +1,8 @@
 import React from 'react'
+import Collapsible from '../Collapsible';
 import './style.css'
 
 export default function AnimalSnapshot({ animal }) {
-    let about = animal.about;
-    let aboutMe = about.split('\.', 1)[0] + ".";
 
   return (
     <div className='snapshot'>
@@ -15,40 +14,9 @@ export default function AnimalSnapshot({ animal }) {
         <p className='kids'>Can live with children: {animal.kids}</p>
         <p className='others'>Can live with other pets: {animal.others}</p>
         <p className='home'>{animal.home}</p>
-        <p className='about'>{aboutMe}</p>
+        <Collapsible closed>
+            {animal.about}
+        </Collapsible>      
     </div>
   )
-}
-
-
-/* import React, { useState } from 'react';
-import useCollapse from 'react-collapsed';
-import './App.css';
-function Collapsible() {
-    const [ isExpanded, setExpanded ] = useState(false);
-    const { getCollapseProps, getToggleProps } = useCollapse({ isExpanded });
-function handleOnClick() {
-        // Do more stuff with the click event!
-        // Or, set isExpanded conditionally 
-        setExpanded(!isExpanded);
-    }
-return (
-        <div className="collapsible">
-            <div className="header" {...getToggleProps({onClick: handleOnClick})}>
-                {isExpanded ? 'Collapse' : 'Expand'}
-            </div>
-            <div {...getCollapseProps()}>
-                <div className="content">
-                    Now you can see the hidden content. <br/><br/>
-                    Click again to hide...
-                </div>
-            </div>
-        </div>
-    );
-}
-function App() {
-    return (
-    <Collapsible/>
-    );
-}
-export default App; */
+};
---
+++ client/src/components/Collapsible/index.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import AnimalSnapshot from "../AnimalProfile";
+
+const Collapsible = ({ open, children }) => {
+    const [isOpen, setIsOpen] = useState(open);
+  
+    const handleFilterOpening = () => {
+      setIsOpen((prev) => !prev);
+    };
+
+
+  return (
+    <div className='collapsible'>
+            <button className='seeMore' onClick={handleFilterOpening}>
+              {!isOpen  ? 'See More' : 'See Less' }</button>
+                
+              <div>{isOpen && <div className="about">{children}</div>}</div>
+    </div>
+  )
+}
+
+    
+
+export default Collapsible;
