5e0ece556235633829971b5c0818a326461f67f8
Ikenna <ikennaagada@gmail.com>
2022-05-25 12:22:13 +0100

    Post migrations

--- client/src/components/AnimalProfile/index.js
+++ client/src/components/AnimalProfile/index.js
@@ -12,7 +12,7 @@ export default function AnimalSnapshot({ animal }) {
     <div className="snapshot">
       <h2 className="name">{animal.name}</h2>
       <img
-        src="https://st3.depositphotos.com/23594922/31822/v/600/depositphotos_318221368-stock-illustration-missing-picture-page-for-website.jpg"
+        src={animal.image}
         alt={animal.name}
       />
       <div>
