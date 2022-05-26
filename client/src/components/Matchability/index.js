78190450f8fb35eaec10fa08eabac64ba1afb316
Jaxsan Sivanesan <jaxsan.sivanesan@gmail.com>
2022-05-26 21:57:16 +0100

    Changed styling for matchability

--- client/src/components/Matchability/index.js
+++ client/src/components/Matchability/index.js
@@ -4,7 +4,27 @@ import './style.css'
 export default function Matchability({score}) {
   const percentage = (100/8)*score
 
+  let renderPercentage;
+
+    if (percentage>66){
+      renderPercentage=(
+        <div className="res-sphereGreen">{percentage}% match!</div>
+      )
+    } else if (33<percentage && percentage<66){
+      renderPercentage=(
+        <div className="res-sphereYellow">{percentage}% match!</div>
+      )
+    } else if (percentage<33){
+      renderPercentage= (
+        <div className="res-sphereRed">{percentage}% match!</div>
+      )
+  
+    }
+  
+
   return (
-    <div className='score'>{percentage}% match!</div>
+    <>
+    {renderPercentage}
+    </>
   )
 }
